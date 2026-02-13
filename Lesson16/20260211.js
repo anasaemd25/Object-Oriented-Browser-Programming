7ce355b1e8723628ec37dd6f4b48c0bd7ce954a9
anasaemd25 <anas.aemd@gmail.com>
2026-02-13 16:18:28 +0200

    Add new HTML and JavaScript files for DOM manipulation exercises and alert functionality, and solver Assigment-4

--- Lesson16/20260211.js
+++ Lesson16/20260211.js
@@ -1,1 +1,14 @@
 alert("Hello from first");
+
+// Access the element by ID 
+const heading = document.getElementById("heading1");
+
+// Change the text
+heading.textContent = "New title";
+
+// Change the style (as in your HTML comment)
+heading.setAttribute("style", "color: red; margin: 50px;");
+
+// Access the input and change its value
+const input = document.getElementById("name-input");
+input.value = "Hello World";
